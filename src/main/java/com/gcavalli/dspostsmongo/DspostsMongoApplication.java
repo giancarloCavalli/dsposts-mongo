05afad776705afd96b39a88c24a3e6a1e0ec25ca
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-10-30 11:22:19 -0300

    Initial commit

---
+++ src/main/java/com/gcavalli/dspostsmongo/DspostsMongoApplication.java
@@ -0,0 +1,13 @@
+package com.gcavalli.dspostsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspostsMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspostsMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gcavalli/dspostsmongo/DspostsMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcavalli.dspostsmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspostsMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
